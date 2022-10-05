ebf56c5a8e7b959935325eb41edc97aaacf6bae0
aevc07 <aevc07@gmail.com>
2022-10-05 08:08:10 -0300

    actualización de elementos

--- chivoinfinito.js
+++ chivoinfinito.js
@@ -1,4 +1,31 @@
-console.log("chivo de guerra")
+var cacaBubu = [
+    {principal: "llorones", resultado: "lepe"},
+    {principal: "desordenados", resultado: "coñazo"},
+    {principal: "llorones", resultado: "agua fria"},
+    {principal: "desordenados", resultado: "leones"}
+]
+var NewCacaBubu = cacaBubu.map(function(cacaBubu) {return cacaBubu.resultado + " merecido pues"})
+console.log(NewCacaBubu)
+
+var Comidas = [
+{helados: "crema e cacahuate", entrada: "arepa"},
+{helados: "agua e coco", entrada: "tequeños de eso"},
+{helados: "maní con alcohol", entrada: "guaracha e jipi"},
+{helados: "agua e coco", entrada: "miche e teca"}
+]
+var LaRataCome = Comidas.filter(function(comida) {return comida.helados === "agua e coco"})
+console.log(LaRataCome)
+
+var Comidas = [
+    {helados: "crema e cacahuate", entrada: "arepa"},
+    {helados: "agua e coco", entrada: "tequeños de eso"},
+    {helados: "maní con alcohol", entrada: "guaracha e jipi"},
+    {helados: "agua e coco", entrada: "miche e teca"}
+    ]
+    var LaRataCome = Comidas.filter(function(comida) {return comida.helados === "agua e coco"}).map(function(comida) {return comida.entrada})
+    console.log(LaRataCome)
+    
+
 
 
 
--- toripollo.js
+++ toripollo.js
@@ -1,2 +1,4 @@
 var LandraBrava = "menol de calacas la landra tan brava"
 console.log(LandraBrava)
+var números = [23, 43, 10, 3, 9, 5, 13, 12, 65]
+var CuentaNúmeros = números.forEach(function(número, i) {console.log(`el número ${número} se encuentra en el indice ${i}`);},)
