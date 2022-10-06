40394141178d89b3d47945e2c647afbadd2db632
aevc07 <aevc07@gmail.com>
2022-10-06 09:07:28 -0300

    actualizaciones right now bro

--- arrays.js
+++ arrays.js
@@ -1,5 +1,25 @@
 var CabezadePollo = "los toripollos del tomorow"
-console.log(CabezadePollo + "que demencia bro")
+console.log(CabezadePollo.toUpperCase() + " que demencia bro dís créizii pípol menool")
 
+var números = [23, 43, 10, 3, 9, 5, 13, 12, 65, 56, 33, 44, 33, 234, 453456]
+console.log(números.sort(function(prev,next) {return prev - next}))
+
+var AbecedariusFatis = ["V", "S", "H", "O", "F", "D", "C", "W", "Q"]
+console.log(AbecedariusFatis.sort())
+
+var OrdenPues = ["naguará",
+"epale",
+"chachara",
+"pepe",
+"carajos"]
+console.log(OrdenPues.sort())
+
+var canciones = [
+    {name: "Paint it black", track: 5},
+     {name: "Desert rose", track: 10},
+      {name: "Le pica", track: 4}
+]
+var NuevosDatos = canciones.some(function(element) {return element = 5})
+console.log(NuevosDatos)
 
 
--- chivoDerDestino.js
+++ chivoDerDestino.js
@@ -1,1 +1,17 @@
-console.log("war machine")
+var MetalForce = "Y ustedes que esperaban PERROOOS naiden jíar va a tu dáuuunN"
+console.log(MetalForce.toLowerCase())
+
+var TutiMayus = "ai am da uán in dis pleiiiizzzz"
+console.log(TutiMayus.toUpperCase())
+
+var LePípolChimba = "HOY TODO MUNDO ACOSTUMBRA A DEJARTE A MITAD DE CONVERSA, PERO YO NO HAGO ESO, OF CÓURS NÓT"
+console.log(LePípolChimba.toLowerCase())
+
+var números = [23, 43, 10, 3, 9, 5, 13, 12, 65]
+console.log(números.find(function(element) {return element === 12}))
+
+var números = [23, 43, 10, 3, 9, 5, 13, 12, 65]
+console.log(números.indexOf(13))
+
+var random = ["rojo", "verde", "rayos", "fiuchár", "ráit", "énglíssshh"]
+console.log(random.indexOf("rayos"))
