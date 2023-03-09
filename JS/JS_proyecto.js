1c13150b39c2a1e2030ea3f35dc710867853eeff
aevc07 <aevc07@gmail.com>
2023-03-08 23:10:35 -0300

    PROYECTO FINAL ENTREGA

---
+++ JS/JS_proyecto.js
@@ -0,0 +1,10 @@
+document.getElementById("fecha").addEventListener("click", function(){
+    alert("29 de marzo de 1785");
+});
+
+function mouseOver() {
+    document.getElementById("video").style.background = "limegreen";
+}
+function mouseOut() {
+    document.getElementById("video").style.background = "lightyellow";
+}
---
+++ JS/Power.js
@@ -0,0 +1,14 @@
+document.getElementById("Power").addEventListener("click", function(){
+    alert("Creación e implementación de cortafuegos y rediseño de sitios web");
+});
+
+document.getElementById("Response").addEventListener("click", function(){
+    alert("Maquetación mediante herramientas CSS y diseño y desarrollo");
+});
+
+function mouseOver() {
+    document.getElementById("estilo").style.background = "yellow";
+}
+function mouseOut() {
+    document.getElementById("estilo").style.background = "cornflowerblue";
+}
